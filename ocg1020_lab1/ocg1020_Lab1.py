b1b7ac642bd612ad2b5529954681528cbb01ca74
Bismmo1 <71271769+Bismmo1@users.noreply.github.com>
2026-01-27 15:16:34 -0500

    Refactor to add checkerboard drawing functions

--- ocg1020_lab1/ocg1020_Lab1.py
+++ ocg1020_lab1/ocg1020_Lab1.py
@@ -1,4 +1,4 @@
-###CS 2370 labs.  
+###CS 2370 labs.
 ## Ollie Grazier
 
 def print_checker_boundary_row():
@@ -6,16 +6,54 @@ def print_checker_boundary_row():
 
 def print_checker_boundary_column():
     print("| | | | | | | | |")
-    
+
 def print_checker_spaces_under_boundary():
     print("+-+-+-+-+-+-+-+-+")
+    
+def draw_checker_board():
+  for _ in range(4):
+      print_checker_boundary_row()
+      print_checker_boundary_column()
 
 def main():
 
-    print_checker_boundary_row()
-    print_checker_boundary_column()
-    
-for _ in range(4):
-    main()
-    
-print_checker_spaces_under_boundary()
+    print('Running the tests...')
+    print() #empty line
+    draw_checker_board()
+    print_checker_spaces_under_boundary()
+
+main()
+print()
+print("=-=-=-=-=-=-=-=-=")
+print()
+
+##checkerboard with pieces
+def checker_x_0():
+  print("+-+-+-+-+-+-+-+-+")
+  print("|x| |x| |x| |x| |")
+
+def checker_x_1():
+  print("+-+-+-+-+-+-+-+-+")
+  print("| |x| |x| |x| |x|")
+
+def checker_o_0():
+  print("+-+-+-+-+-+-+-+-+")
+  print("|o| |o| |o| |o| |")
+
+def checker_o_1():
+  print("+-+-+-+-+-+-+-+-+")
+  print("| |o| |o| |o| |o|")
+
+def draw_checker_board_with_pieces():
+  checker_x_1()
+  checker_x_0()
+  checker_x_1()
+  for _ in range(2):
+      print_checker_boundary_row()
+      print_checker_boundary_column()
+  checker_o_0()
+  checker_o_1()
+  checker_o_0()
+
+
+draw_checker_board_with_pieces()
