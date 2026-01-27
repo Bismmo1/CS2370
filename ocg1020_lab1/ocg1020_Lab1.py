51e5e1ee3f6d594aa8487b589afdbcc126eebb71
Bismmo1 <71271769+Bismmo1@users.noreply.github.com>
2026-01-27 16:49:45 -0500

    Refactor checkerboard and draughts board functions
    Refactor checkerboard and draughts board functions for clarity and functionality.

--- ocg1020_lab1/ocg1020_Lab1.py
+++ ocg1020_lab1/ocg1020_Lab1.py
@@ -1,6 +1,8 @@
-###CS 2370 lab1
-## Ollie Grazier
-## Contains both the standard checkerboard
+"""CS 2370 labs."""
+"""Ollie Grazier"""
+
+
+##checker Board
 def print_checker_boundary_row():
     print("+-+-+-+-+-+-+-+-+")
 
@@ -11,23 +13,12 @@ def print_checker_spaces_under_boundary():
     print("+-+-+-+-+-+-+-+-+")
     
 def draw_checker_board():
-  for _ in range(4):
+  for _ in range(8):
       print_checker_boundary_row()
       print_checker_boundary_column()
 
-def main():
 
-    print('Running the tests...')
-    print() #empty line
-    draw_checker_board()
-    print_checker_spaces_under_boundary()
-
-main()
-print()
-print("=-=-=-=-=-=-=-=-=")
-print()
-
-##checkerboard with pieces
+##checkerboard pieces
 def checker_x_0():
   print("+-+-+-+-+-+-+-+-+")
   print("|x| |x| |x| |x| |")
@@ -44,6 +35,7 @@ def checker_o_1():
   print("+-+-+-+-+-+-+-+-+")
   print("| |o| |o| |o| |o|")
 
+#create checkerboard with pieces
 def draw_checker_board_with_pieces():
   checker_x_1()
   checker_x_0()
@@ -55,7 +47,45 @@ def draw_checker_board_with_pieces():
   checker_o_1()
   checker_o_0()
 
+## Draughts Functions
 
-draw_checker_board_with_pieces()
+def print_draughts_space_row():
+  print("+-+-+-+-+-+-+-+-+-+-+")
 
+def print_draughts_space_column():
+  print("| | | | | | | | | | |")
 
+def print_draughts_spaces_under_boundary():
+  print("+-+-+-+-+-+-+-+-+-+-+")
+  
+## draughts board creation
+def draw_draughts_board():
+  for _ in range(10):
+      print_draughts_space_row()
+      print_draughts_space_column()
+  print_draughts_spaces_under_boundary()
+
+def main():
+
+    print('Running the tests...')
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print()
+    ##draw Checkerboard
+    draw_checker_board()
+    print_checker_spaces_under_boundary()
+    print()
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print()
+    ##draw the checkerboard with pieces
+    draw_checker_board_with_pieces()
+    print_checker_spaces_under_boundary()
+    print()
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    print()
+    ##draw the draughts board
+    draw_draughts_board()
+    print_draughts_spaces_under_boundary()
+
+## Im not sure how this works but I will ask in class tomorrow
+if __name__ == "__main__":
+    main()
