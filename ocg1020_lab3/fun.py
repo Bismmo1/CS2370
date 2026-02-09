82d97e6455bdf5c73189e625881d59152bc9ffa3
Bismmo1 <olivergrazier@gmail.com>
2026-02-09 15:17:09 -0500

    lab3 start

---
+++ ocg1020_lab3/fun.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+screen = turtle.Screen() #provides a canvas to draw on
+
+t1 = turtle.Turtle()
+
+for x in range (1000):
+    t1.forward(25)
+    t1.right(111)
+    t1.forward(5)
+    t1.left(47)
