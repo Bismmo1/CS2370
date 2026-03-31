4d6e8c4a990e80e1cd2386aef1adcfd6a4017d14
Bismmo1 <71271769+Bismmo1@users.noreply.github.com>
2026-03-31 11:53:05 -0400

    Enhance code readability with comments
    Added comments to clarify the purpose of variables and functions.

--- ocg1020_lab8.py
+++ ocg1020_lab8.py
@@ -1,11 +1,13 @@
 #ocg1020.py
 #Lab 8
 
-numList = [1,5, 2, 3, 5, 6]
+
+#I made two lists, one of numbers, and another of words
+numList = [1,5, 2, 3, 5, 6,]
 strList = ['hello', 'friend', 'there', 'hello','bababooey', 'hello', 'there', 'hello', 'friend', 'what']
 #Part 1
 def get_sum(numList):
-    total = 0
+    total = 0#for this I created a total, then checked if i would be greater than the current number in total
     for i in range(len(numList)):
         total += numList[i]
         #print(total)
@@ -14,7 +16,7 @@ def get_sum(numList):
     
 #Part 2
 def sum_even(numList):
-    totalEven = 0
+    totalEven = 0#first I checked if the num was even by % 2, if so then added it to totalEven
     for i in range(len(numList)):
         if numList[i] % 2 == 0:
             totalEven += numList[i]
@@ -26,7 +28,7 @@ def sum_even(numList):
 
 #Part 3
 def sum_odd(numList):
-    totalOdd = 0
+    totalOdd = 0#similar to sum_even, I simply swapped the if from == to !=, allowing it to check for non even numbers
     for i in range(len(numList)):
         if numList[i] % 2 != 0:
             totalOdd += numList[i]
@@ -38,7 +40,7 @@ def sum_odd(numList):
 
 #Part 4
 def get_max(numList):
-    max = 0
+    max = 0#For this one I compared the current number to the stored largest number in max, if it was larger the new number would take its place in max
     for i in range(len(numList)):
         if numList[i] > max:
             max = numList[i]
@@ -50,6 +52,9 @@ def get_max(numList):
 #Part 5
 def CountWords(str):
     word_count = {}
+    #I did this one a little different than the instructions and I hope this is okay. I used a string list instead of a string like in the example
+    #I then check using an if statement if the word was added to a new dictonaryif so then it would add += 1 to the count of that word in the dictionary,
+    #if not then it would add it to the dictionary and raise the count by 1. seen in the else: statement
     for word in str:
         if word in word_count:
             word_count[word] += 1
@@ -75,6 +80,8 @@ def get_longest(strList):
 
 
 def main():
+    
+    
     print("Part 1: get_sum")
     print(get_sum(numList))
     print('======================')
