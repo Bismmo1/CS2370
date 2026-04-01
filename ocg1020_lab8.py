1fb90d0b69dbeddb6f515d998614f10391379c77
Bismmo1 <71271769+Bismmo1@users.noreply.github.com>
2026-04-01 15:34:09 -0400

    Refactor functions and update variable names

--- ocg1020_lab8.py
+++ ocg1020_lab8.py
@@ -2,12 +2,11 @@
 #Lab 8
 
 
-#I made two lists, one of numbers, and another of words
-numList = [1,5, 2, 3, 5, 6,]
-strList = ['hello', 'friend', 'there', 'hello','bababooey', 'hello', 'there', 'hello', 'friend', 'what']
+
 #Part 1
 def get_sum(numList):
     total = 0#for this I created a total, then checked if i would be greater than the current number in total
+
     for i in range(len(numList)):
         total += numList[i]
         #print(total)
@@ -17,71 +16,111 @@ def get_sum(numList):
 #Part 2
 def sum_even(numList):
     totalEven = 0#first I checked if the num was even by % 2, if so then added it to totalEven
+
     for i in range(len(numList)):
         if numList[i] % 2 == 0:
             totalEven += numList[i]
-            '''print(totalEven)
+            #I created else statements for testing almost all functions, these can be uncommented
+            #You can see logical explainations
+            '''print(totalEven)'''
         else:
-            print("Odd number, skipping", numList[i])'''
+            '''print("Odd number, skipping", numList[i])'''
+            pass
     
     return totalEven
 
 #Part 3
 def sum_odd(numList):
     totalOdd = 0#similar to sum_even, I simply swapped the if from == to !=, allowing it to check for non even numbers
+
     for i in range(len(numList)):
         if numList[i] % 2 != 0:
             totalOdd += numList[i]
-            '''print(totalOdd)
+            '''print(totalOdd)'''
         else:
-            print("Even number, skipping", numList[i])'''
+            '''print("Even number, skipping", numList[i])'''
+            pass
     
     return totalOdd
 
 #Part 4
 def get_max(numList):
     max = 0#For this one I compared the current number to the stored largest number in max, if it was larger the new number would take its place in max
+
     for i in range(len(numList)):
         if numList[i] > max:
             max = numList[i]
-            '''print(max)
+            '''print(max)'''
         else:
-            print(numList[i] ,"is not larger than", max)'''
+            '''print(numList[i] ,"is not larger than", max)'''
+            pass
     return max
 
 #Part 5
-def CountWords(str):
+def CountWords(string):
+    #create a temp dictionary, this will eventually hold all your words and counts
     word_count = {}
-    #I did this one a little different than the instructions and I hope this is okay. I used a string list instead of a string like in the example
-    #I then check using an if statement if the word was added to a new dictonaryif so then it would add += 1 to the count of that word in the dictionary,
-    #if not then it would add it to the dictionary and raise the count by 1. seen in the else: statement
-    for word in str:
+    temp = string.split()#you can split a string into a list using this
+    #print(temp)
+    
+    for word in temp:#read each word in list temp, then compare it to word_count
         if word in word_count:
-            word_count[word] += 1
+            word_count[word] += 1#add a count to that specific word
             
         else:
-            word_count[word] = 1
+            #print("storing", word)
+            word_count[word] = 1#if it does not exist make a new word in word_count
+
+    #Uncomment for another way to print this list
+    '''for a, b in word_count.items():
+        print(f"{a}: {b}", end=" ")#This way we print each part of the dictionary one at a time.
+        
+    '''
+    return word_count#return the completed dictionary
+
+
+'''def CountWords_ocg1020_version(strList):
+    word_count = {}#this is the same as above, except it skips past converting a string into a list
     
-    return word_count
+    for word in string:
+        if word in word_count:
+            word_count[word] += 1
+                
+        else:
+            word_count[word] = 1
+            
+        
+    return word_count'''
+
+
 
 #Part 6
 def get_longest(strList):
-    longest = ""
-    for i in range(len(strList)):
-        if len(strList[i]) > len(longest):
-            longest = strList[i]
-            '''print(longest)
-        else:
-            print(strList[i],  "is not longer than", longest)'''
+    longest = ""#placeholder/temp
 
-    return longest
+    for i in range(len(strList)):
+        if len(strList[i]) > len(longest):#compare current to longest
+            longest = strList[i]
+            '''print(longest)'''
+        else:
+            '''print(strList[i],  "is not longer than", longest)'''
+            pass
+
+    return longest#return longest
         
 
 
 
 def main():
-    
-    
+
+    #Lists
+    numList = [1,5, 2, 3, 5, 6,]
+    list2 = "hello yes there hello hello yes friend yes hello"
+
+
+    #list for personal def version of part 5
+    strList = ['hello', 'friend', 'there', 'hello','bababooey', 'hello', 'there', 'hello', 'friend', 'what']
+        
     print("Part 1: get_sum")
     print(get_sum(numList))
     print('======================')
@@ -99,9 +138,10 @@ def main():
     print('======================')
 
     print("Part 5: CountWords")
-    print(CountWords(strList))
+    print(CountWords(list2))
     print('======================')
     
+
     print("Part 6: get_longest")
     print(get_longest(strList))
     print('======================')
